d3725faf4641a23ac64a7c37cd5e389fe2388d9d
user <palmtree8880@gmail.com>
2022-10-18 22:10:23 +0900

    static

--- sitesSaver/settings.py
+++ sitesSaver/settings.py
@@ -189,7 +189,7 @@ EMAIL_HOST_PASSWORD = ''
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = [
